e9b24d90e98c0a00a95863b6989e97356016e9fe
Krzysztof Podurgiel <s17769@pjwstk.edu.pl>
2020-04-05 23:54:58 +0200

    6.3

--- APBD_cw3/Controllers/StudentsController.cs
+++ APBD_cw3/Controllers/StudentsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using APBD_cw3.Models;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -28,5 +29,13 @@ namespace APBD_cw3.Controllers
 
             return NotFound("Nie znaleziono studenta");
         }
+
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+            return Ok(student);
+        }
+
     }
 }
